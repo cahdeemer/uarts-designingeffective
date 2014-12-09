b06679f9bcd07feb87750f391892e5912b26c260
cahdeemer <cahdeemer@gmail.com>
2014-12-09 13:34:52 -0500

    JavaScript
    Added additional copy and JS. Made adjustment to .bigphoto img needed
    as a result of adding copy and JS.

---
+++ js/bucket.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+
+	// Body Fade In
+	
+	$("body").css("display", "none");
+	$("body").fadeIn(1000,"swing");
+
+
+	// Draggable Example
+	
+	$("#draggable").draggable();
+	
+	// Accordion Navigation
+        
+     $('#nav > li > a').click(function(){
+		if ($(this).attr('class') != 'active'){
+				$('#nav li ul').slideUp();
+				$(this).next().slideToggle();
+				$('#nav li a').removeClass('active');
+				$(this).addClass('active');
+		}
+	});
+	
+	//Toggle Content
+	
+	$('#hideMeTrigger').click(function() {
+		var text;
+
+    	$('#hideMeDiv').toggleClass('active');
+    	if ($('#hideMeDiv').hasClass('active')){
+    		text = 'Show Less';
+    	} else {
+    		text = 'Show More';
+    	}
+
+    	$(this).text(text);
+	    return false;
+    });
+	
+});
